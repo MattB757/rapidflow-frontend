2289e82982ac6042f8ce27962b806ccae3606417
Md. Raju Ahmmed <ahraju2003@gmail.com>
2025-07-03 06:21:35 +0600

    update home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
+import CtaSection from "@/components/cta-section";
+import Features from "@/components/features-section";
+import Footer from "@/components/footer-section";
+import Navbar from "@/components/navbar";
 import Register from "@/components/register";
+import Testimonials from "@/components/testimonials-section";
+import React from "react";
 
-export default function StaterPage() {
+const HomePage = () => {
   return (
     <div>
+      <Navbar />
       <Register />
+      <Features />
+      <Testimonials />
+      <CtaSection />
+      <Footer />
     </div>
   );
-}
+};
+
+export default HomePage;
