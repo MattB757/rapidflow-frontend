46edb3270669366f6f140bcdcd96fecf53d99d21
Raju Ahmmed <ahraju2003@gmail.com>
2025-07-27 05:51:38 +0600

    remove loading page in home page

--- src/components/register.tsx
+++ src/components/register.tsx
@@ -198,7 +198,7 @@ export default function Register() {
               <div className="flex items-center justify-center min-h-[400px]">
                 <div className="text-center">
                   <Loader2 className="w-12 h-12 text-red-400 animate-spin mx-auto mb-4" />
-                  <p className="text-white text-lg">Loading page...</p>
+                  {/* <p className="text-white text-lg">Loading page...</p> */}
                 </div>
               </div>
             ) : (
