63d671802a0f927bafb7ece26df7e69b98144120
MattB757 <blockmatthew757@gmail.com>
2025-11-24 06:12:25 -0500

    add open-next.config.ts for cloudflare build

---
+++ open-next.config.ts
@@ -0,0 +1,10 @@
+import type { OpenNextConfig } from "open-next/types/open-next";
+
+const config: OpenNextConfig = {
+  default: {},
+  middleware: {
+    external: true,
+  },
+};
+
+export default config;
