e31d476d8a486efd9620aaf45de108b51eece634
MattB757 <blockmatthew757@gmail.com>
2025-11-24 22:47:15 -0500

    Rebuild with correct TinaCMS content paths

--- .open-next/cloudflare/init.js
+++ .open-next/cloudflare/init.js
@@ -49,7 +49,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1763992594187,
+    __BUILD_TIMESTAMP_MS__: 1764042222243,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,
